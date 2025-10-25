9aa8539b189ada3ce386eee2343dc2ddd1eecc7b
Programming for Biology <pfb@info04.cshl.edu>
2025-10-25 15:17:22 -0400

    read_re reading through a list

--- read_re.py
+++ read_re.py
@@ -1,7 +1,7 @@
 import re
 import Levenshtein
 
-def re_match(restriction_enzyme):
+def re_match(restriction_enzymes):
     with open('re_w_cutsites.txt','r') as REtable:
         re_dict = {}
         for line in REtable:
@@ -15,22 +15,34 @@ def re_match(restriction_enzyme):
         re_names = []
         for key,val in re_dict.items():
             re_names.append(key)
-    matches = [res_enz for res_enz in re_names if Levenshtein.distance(res_enz, restriction_enzyme) < 3]
-    if not matches :
-        invalid_warning = 'Please enter a valid restriction enzyme. Check out a list of restriction enzymes here: https://rebase.neb.com/rebase/link_bionetc.'
-        return invalid_warning
-    elif restriction_enzyme in matches:
-        return re_dict[restriction_enzyme]
-    elif restriction_enzyme not in matches:
-        separator = ", "
-        my_string = separator.join(matches)
-        suggestion_warning = f'Do you mean {my_string}?'
-        return suggestion_warning
 
-restrictionenz = 'SmaI'
-print(re_match(restrictionenz))
+    matches_list = []
+    for res_enzyme in restriction_enzymes:
+        matches = []
+        for res_enz in re_names: 
+            if Levenshtein.distance(res_enz, res_enzyme) < 2:
+                matches.append(res_enz)
+        matches_list.append(matches)
 
-# def re_dict_names():
+    results = []
+    for count,res_enz_input in enumerate(restriction_enzymes):
+        if not matches_list[count]:
+            invalid_warning = 'Please enter a valid restriction enzyme. Check out a list of restriction enzymes here: https://rebase.neb.com/rebase/link_bionetc.'
+            return invalid_warning
+        elif res_enz_input in matches_list[count]:
+            results.append(re_dict[res_enz_input])
+        elif res_enz_input not in matches_list[count]:
+            separator = ", "
+            my_string = separator.join(matches_list[count])
+            suggestion_warning = f'Do you mean {my_string}?'
+            return suggestion_warning
+    
+    return results
+
+restriction_enzymes = ['ZraI','BamHI','XmaI']
+print(re_match(restriction_enzymes))
+
+# def re_match(restriction_enzyme):
 #     with open('re_w_cutsites.txt','r') as REtable:
 #         re_dict = {}
 #         for line in REtable:
@@ -44,29 +56,24 @@ print(re_match(restrictionenz))
 #         re_names = []
 #         for key,val in re_dict.items():
 #             re_names.append(key)
-#         return re_dict, re_names
+#     matches_list = []
+#     for item in restriction_enzyme:
+#         matches = res_enz for res_enz in re_names if Levenshtein.distance(res_enz, restriction_enzyme) < 3
+#         print(matches)
+#         matches_list.append(matches)
+#     return matches_list
+    
+    # if not matches :
+    #     invalid_warning = 'Please enter a valid restriction enzyme. Check out a list of restriction enzymes here: https://rebase.neb.com/rebase/link_bionetc.'
+    #     return invalid_warning
+    # elif restriction_enzyme in matches:
+    #     return re_dict[restriction_enzyme]
+    # elif restriction_enzyme not in matches:
+    #     separator = ", "
+    #     my_string = separator.join(matches)
+    #     suggestion_warning = f'Do you mean {my_string}?'
+    #     return suggestion_warning
 
-# re_dictionary_namelist = re_dict_names()
-# #This variable is a tuple. The first element is a dictionary {'Enzyme': 'Cutsite', ...} and the second element is a list of res enzymes ['BamHI','EcoRI', ...]
-# print(re_dictionary_namelist[0])
-# print('')
-# print('')
-# print(re_dictionary_namelist[1])
-# print('')
-# print('')
+# restrictionenz = ['BamHI','EcoRI']
 
-# def re_match(restriction_enzyme):
-#     matches = [res_enz for res_enz in re_dictionary_namelist[1] if Levenshtein.distance(res_enz, restriction_enzyme) < 3]
-#     if not matches :
-#         invalid_warning = 'Please enter a valid restriction enzyme. Check out a list of restriction enzymes here: https://rebase.neb.com/rebase/link_bionetc.'
-#         return invalid_warning
-#     elif restriction_enzyme in matches:
-#         return re_dictionary_namelist[0][restriction_enzyme]
-#     elif restriction_enzyme not in matches:
-#         separator = ", "
-#         my_string = separator.join(matches)
-#         suggestion_warning = f'Do you mean {my_string}?'
-#         return suggestion_warning
-
-# restrictionenz = 'BamHSJKHSFKI'
 # print(re_match(restrictionenz))
